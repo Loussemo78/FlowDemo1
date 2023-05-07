451717b0be21983548570d8cd032890393492e80
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-07 18:31:58 +0200

    flow succesfully emit on the textView

--- app/src/main/java/com/example/flowdemo1/MainActivity.kt
+++ app/src/main/java/com/example/flowdemo1/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.flowdemo1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,15 +19,15 @@ class MainActivity : AppCompatActivity() {
         //PRODUCER DATA STREAMS
         val myFlow = flow<Int> {
             for (i in 1..100){
-                emit(1)
+                emit(i)
                 delay(1000L)
             }
         }
-      
+      val textView = findViewById<TextView>(R.id.tvResult)
        //CONSUMER
         CoroutineScope(Dispatchers.Main).launch {
             myFlow.collect{
-                Log.i("MYTAG", "Current index is $it")
+                textView.text = "Current index is $it"
             }
         }
     }
